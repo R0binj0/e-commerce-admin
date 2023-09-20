58f693a206b81cc9f314bc16e2ff7ce81f5d9c88
R0binj0 <robin.pruunlep@ametikool.ee>
2023-09-20 14:11:35 +0300

    commit

--- app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
@@ -19,7 +19,7 @@ export const columns: ColumnDef<ColorColumn>[] = [
     accessorKey: "value",
     header: "Value",
     cell: ({ row }) => [
-      <div className="flex items-center gap-x-2">
+      <div key={`value-${row.id}`} className="flex items-center gap-x-2">
         {row.original.value}
         <div className="h-6 w-6 rounded-full border" style={{backgroundColor: row.original.value}}></div>
       </div>
